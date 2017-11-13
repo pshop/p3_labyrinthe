d30c97a3d7f0b75ebf6784fa4b2281efd03dd3d2
pshop <choppin.p@gmail.com>
2017-11-13 13:02:02 +0100

    fichier main(app_macgyver) + objets.py + ClassLabyrinthe.py

--- ClassLabyrinthe.py
+++ ClassLabyrinthe.py
@@ -3,31 +3,34 @@ import json
 class Labyrinthe:
 
     def __init__(self, file, key):
+        #load the labyrinthe from a json external file
         with open(file) as f:
             data = json.load(f)
             for entry in data:
                 self.laby = entry[key]
 
-    @property
-    def empy_spaces(self):
-
-        empty_coordonates = []
+    def search_items(self, item):
+        items_coordonates = []
         #fils the list empty_coordonates
         for line_nb, line_value in enumerate(self.laby):
             for sprite_nb, sprite_value in enumerate(line_value):
                 pos = (line_nb, sprite_nb)
                 #if the pos is free, i fell the empty_coordonates
-                if sprite_value == " ":
-                    empty_coordonates.append(pos)
-        return empty_coordonates
+                if sprite_value == item:
+                    items_coordonates.append(pos)
+        return items_coordonates
+
+    @property
+    def empy_spaces(self):
+        return self.search_items(" ")
 
     @property
     def macgyver_coord(self):
-        pass
+        return self.search_items("X")
 
     @property
     def exit_coord(self):
-        pass
+        return self.search_items("E")
 
     def print_laby(self):
         #print the entire lab
@@ -37,10 +40,5 @@ class Labyrinthe:
                 print(sprite, end = "")
             print()
 
-def main():
-    labyrinthe1 = Labyrinthe("laby_setup.json", "laby")
-    empty_pos = labyrinthe1.empy_spaces
-    labyrinthe1.print_laby()
-    print(empty_pos)
-
-main()
+if __name__ == "__main__":
+    pass
---
+++ app_macgyver.py
@@ -0,0 +1,7 @@
+from ClassLabyrinthe import *
+
+labyrinthe1 = Labyrinthe("laby_setup.json", "laby")
+empty_pos = labyrinthe1.empy_spaces
+labyrinthe1.print_laby()
+print(labyrinthe1.macgyver_coord)
+print(labyrinthe1.exit_coord)
